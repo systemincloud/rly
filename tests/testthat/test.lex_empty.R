005a5a87c0308e5bf1e33a5eeba956d41f0cd6eb
mji <marek.jagielski@gmail.com>
2016-06-27 22:17:30 +0200

    test : lex empty

---
+++ tests/testthat/test.lex_empty.R
@@ -0,0 +1,17 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("No rules defined")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS')
+  )
+)
+
+test_that("missing regex", {
+  results <- capture.output(rly::lex(Lexer))[[1]]
+  expect_equal(results, "DEBUG>  No rules of the form t_rulename are defined ")
+})
