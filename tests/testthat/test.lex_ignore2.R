ae6d3f3bab6326e2f368ab51ee72198206939cbd
mji <marek.jagielski@gmail.com>
2016-06-28 23:29:44 +0200

    test.lex_ignore2

---
+++ tests/testthat/test.lex_ignore2.R
@@ -0,0 +1,23 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("ignore declaration as a raw string")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS'),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = '\\d+',
+    t_ignore = ' \t',
+    t_error = function(t) {}
+  )
+)
+
+test_that("t_ignore improper", {
+  rly::lex(Lexer)
+})
+
+
