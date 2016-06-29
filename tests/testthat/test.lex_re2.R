fbfd7d0aafe445483a4d8803fdb4bd54bc6aaa69
mji <marek.jagielski@gmail.com>
2016-06-30 00:46:25 +0200

    test.lex_re2.R

---
+++ tests/testthat/test.lex_re2.R
@@ -0,0 +1,20 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("Regular expression rule matches empty string")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS'),
+    t_PLUS = '\\+?',
+    t_MINUS = '-',
+    t_NUMBER = '\\d+',
+    t_error = function(t) { }
+  )
+)
+
+test_that("regex matches empty string", {
+  expect_output(rly::lex(Lexer), "DEBUG>  Regular expression for rule 't_PLUS' matches empty string ")
+})
