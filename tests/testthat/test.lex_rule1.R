161a3a0ad7c28e8d814ba41faadea5a0aa420ec9
mji <marek.jagielski@gmail.com>
2016-06-30 00:55:43 +0200

    test.lex_rule1

---
+++ tests/testthat/test.lex_rule1.R
@@ -0,0 +1,21 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("Rule incorrect")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS'),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = 1,
+    t_error = function(t) { }
+  )
+)
+
+test_that("rule incorrect", {
+  expect_output(rly::lex(Lexer), "DEBUG>  t_NUMBER not defined as a function or string ")
+})
+
