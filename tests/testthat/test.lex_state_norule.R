56ee7213e70b944a2779d73633bd2aa7388241ef
mji <marek.jagielski@gmail.com>
2016-07-01 00:50:17 +0200

    reformat

--- tests/testthat/test.lex_state_norule.R
+++ tests/testthat/test.lex_state_norule.R
@@ -6,21 +6,21 @@ library(rly)
 context("Declaration of a state for which no rules are defined")
 
 Lexer <- R6Class("Lexer",
-    public = list(
-        tokens = c('NUMBER', 'PLUS','MINUS'),
-        states = list(c('comment', 'exclusive'),
-                      c('example', 'exclusive')),
-        t_PLUS = '\\+',
-        t_MINUS = '-',
-        t_NUMBER = '\\d+',
-        t_comment = function(re='/\\*', t) {
-          t$lexer$begin('comment')
-        },
-        t_comment_body_part = function(re='(.|\\n)*\\*/',  t) {
-          t$lexer$begin('INITIAL')
-        },
-        t_error = function(t) { }
-    )
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS'),
+    states = list(c('comment', 'exclusive'),
+                  c('example', 'exclusive')),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = '\\d+',
+    t_comment = function(re='/\\*', t) {
+      t$lexer$begin('comment')
+    },
+    t_comment_body_part = function(re='(.|\\n)*\\*/',  t) {
+      t$lexer$begin('INITIAL')
+    },
+    t_error = function(t) { }
+  )
 )
 
 test_that("no rule at all for state", {
