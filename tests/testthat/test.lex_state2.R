6c8fbacc2d5d69e2fb7d9720ff9e17861772e99d
mji <marek.jagielski@gmail.com>
2016-07-02 22:39:46 +0200

    test.lex_re2.R

---
+++ tests/testthat/test.lex_state2.R
@@ -0,0 +1,31 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("Bad state declaration")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS'),
+    states = list(c('comment', 'comment')),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = '\\d+',
+    t_ignore = " \t",
+    t_comment = function(re='/\\*', t) {
+      cat('comment\n')
+      t$lexer$begin('Entering comment state')
+    },
+    t_comment_body_part = function(re='(.|\\n)*\\*/',  t) {
+      cat(sprintf("comment body %s\n", t$value))
+      t$lexer$begin('INITIAL')
+    },
+    t_error = function(t) { }
+  )
+)
+
+test_that("comment", {
+  expect_output(rly::lex(Lexer), "DEBUG>  State type for state must be 'inclusive' or 'exclusive' ",
+                fixed=TRUE)
+    })
