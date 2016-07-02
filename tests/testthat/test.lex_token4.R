3178b0e7a3dae5cbbb99ec6596ca2f926a951a81
mji <marek.jagielski@gmail.com>
2016-07-03 00:20:28 +0200

    test.lex_token4

---
+++ tests/testthat/test.lex_token4.R
@@ -0,0 +1,22 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("Bad token name")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS', '-', 'MINUS'),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = '\\d+',
+    t_ignore = " \t",
+    t_error = function(t) { }
+  )
+)
+
+test_that("comment", {
+  expect_output(rly::lex(Lexer), "ERROR>  Bad token name '-' ",
+                fixed=TRUE)
+})
