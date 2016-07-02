bdb7e9e729f2406a13d725c3d8536314b3d4e309
mji <marek.jagielski@gmail.com>
2016-07-03 01:12:14 +0200

    test.lex_token5

---
+++ tests/testthat/test.lex_token5.R
@@ -0,0 +1,29 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("Return a bad token name")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS', 'MINUS'),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = function(re='\\d+', t) {
+      t$type = "NUM"
+      return(t)
+    },
+    t_ignore = " \t",
+    t_error = function(t) {
+      cat("XXXX")
+      return(t)
+    }
+  )
+)
+
+test_that("bad token type", {
+  lexer <- rly::lex(Lexer)
+  lexer$input("1234")
+  expect_error(lexer$token(), "ERROR> Rule 't_NUMBER' returned an unknown token type 'NUM'", fixed=TRUE)
+})
