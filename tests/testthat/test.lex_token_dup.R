7cca5edb89688d571d6550137f84017743790fe5
mji <marek.jagielski@gmail.com>
2016-07-02 22:57:34 +0200

    test.lex_token_dup.R

---
+++ tests/testthat/test.lex_token_dup.R
@@ -0,0 +1,24 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("Duplicate token name in tokens")
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = c('NUMBER', 'PLUS','MINUS', 'NUMBER'),
+    t_PLUS = '\\+',
+    t_MINUS = '-',
+    t_NUMBER = function(re='\\d+', t) {
+      return(t)
+    },
+    t_ignore = " \t",
+    t_error = function(t) { }
+  )
+)
+
+test_that("comment", {
+  expect_output(rly::lex(Lexer), "DEBUG>  Token 'NUMBER' multiply defined ",
+                fixed=TRUE)
+})
