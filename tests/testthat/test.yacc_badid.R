cd1d85f0f51b8c72466c15491c3d63de018cc558
mji <marek.jagielski@gmail.com>
2016-08-07 17:34:25 +0200

    test.yacc_badid.R

--- tests/testthat/test.yacc_badid.R
+++ tests/testthat/test.yacc_badid.R
@@ -3,11 +3,9 @@
 library(testthat)
 library(rly)
 
-source("test.calclex.R")
-
 context("Attempt to define a rule with a bad-identifier name")
 
-Parser <- R6Class("Parser",
+Parser1 <- R6Class("Parser1",
   public = list(
     tokens = c('NAME','NUMBER', 'PLUS','MINUS','TIMES','DIVIDE','EQUALS', 'LPAREN','RPAREN'),
     # Parsing rules
@@ -23,7 +21,6 @@ Parser <- R6Class("Parser",
       cat(t[1])
     },
     p_statement_expr2 = function(doc='statement : bad&rule', t) { },
-    p_badrule = function(doc='bad&rule : expression', t) { },
     p_expression_binop = function(doc='expression : expression PLUS expression
                                      | expression MINUS expression
                                      | expression TIMES expression
@@ -51,6 +48,51 @@ Parser <- R6Class("Parser",
   )
 )
 
+Parser2 <- R6Class("Parser2",
+  public = list(
+    tokens = c('NAME','NUMBER', 'PLUS','MINUS','TIMES','DIVIDE','EQUALS', 'LPAREN','RPAREN'),
+    # Parsing rules
+    precedence = list(c('left','PLUS','MINUS'),
+      c('left','TIMES','DIVIDE'),
+      c('right','UMINUS')),
+    # dictionary of names
+    names = new.env(hash=TRUE),
+    p_statement_assign = function(doc='statement : NAME EQUALS expression', t) {
+      names[t[1]] <- t[3]
+    },
+    p_statement_expr = function(doc='statement : expression', t) {
+      cat(t[1])
+    },
+    p_badrule = function(doc='bad&rule : expression', t) { },
+    p_expression_binop = function(doc='expression : expression PLUS expression
+        | expression MINUS expression
+        | expression TIMES expression
+        | expression DIVIDE expression', t) {
+      if(t[2] == '+')      t[0] <- t[1] + t[3]
+      else if(t[2] == '-') t[0] <- t[1] - t[3]
+      else if(t[2] == '*') t[0] <- t[1] * t[3]
+      else if(t[2] == '/') t[0] <- t[1] / t[3]
+    },
+    p_expression_uminus = function(doc='expression : MINUS expression %prec UMINUS', t) {
+      t[0] <- -t[2]
+    },
+    p_expression_group = function(doc='expression : LPAREN expression RPAREN', t) {
+      t[0] <- t[2]
+    },
+    p_expression_number = function(doc='expression : NUMBER', t) {
+      t[0] <- t[1]
+    },
+    p_expression_name = function(doc='expression : NAME', t) {
+      t[0] <- names[t[1]]
+    },
+    p_error = function(t) {
+      cat(sprintf("Syntax error at '%s'", t$value))
+    }
+  )
+)
+
+
 test_that("rule name", {
-  expect_error(rly::yacc(Parser), "ERROR> p_statement_expr2: Illegal name bad&rule in rule statement")
+  expect_error(rly::yacc(Parser1), "ERROR> p_statement_expr2: Illegal name bad&rule in rule statement")
+  expect_error(rly::yacc(Parser2), "ERROR> p_badrule: Illegal rule name bad&rule")
 })
