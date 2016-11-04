a630316f290d42a9d92d126108fd06c0ce996773
mji <marek.jagielski@gmail.com>
2016-11-04 13:36:21 +0100

    test.yacc_simple.R

---
+++ tests/testthat/test.yacc_simple.R
@@ -0,0 +1,52 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(rly)
+
+context("A grammar, but we forgot to import the tokens list")
+
+Parser <- R6Class("Parser",
+  public = list(
+    tokens = c('NAME','NUMBER', 'PLUS','MINUS','TIMES','DIVIDE','EQUALS', 'LPAREN','RPAREN'),
+    # Parsing rules
+    precedence = list(c('left','PLUS','MINUS'),
+                      c('left','TIMES','DIVIDE'),
+                      c('right','UMINUS')),
+    # dictionary of names
+    names = new.env(hash=TRUE),
+    p_statement_assign = function(doc='statement : NAME EQUALS expression', t) {
+      names[t[1]] <- t[3]
+    },
+    p_statement_expr = function(doc='statement : expression', t) {
+      cat(t[1])
+    },
+    p_expression_binop = function(doc='expression : expression PLUS expression
+                                                  | expression MINUS expression
+                                                  | expression TIMES expression
+                                                  | expression DIVIDE expression', t) {
+      if(t[2] == '+')      t[0] <- t[1] + t[3]
+      else if(t[2] == '-') t[0] <- t[1] - t[3]
+      else if(t[2] == '*') t[0] <- t[1] * t[3]
+      else if(t[2] == '/') t[0] <- t[1] / t[3]
+    },
+    p_expression_uminus = function(doc='expression : MINUS expression %prec UMINUS', t) {
+      t[0] <- -t[2]
+    },
+    p_expression_group = function(doc='expression : LPAREN expression RPAREN', t) {
+      t[0] <- t[2]
+    },
+    p_expression_number = function(doc='expression : NUMBER', t) {
+      t[0] <- t[1]
+    },
+    p_expression_name = function(doc='expression : NAME', t) {
+      t[0] <- names[t[1]]
+    },
+    p_error = function(t) {
+      cat(sprintf("Syntax error at '%s'", t$value))
+    }
+  )
+)
+
+test_that("token", {
+  rly::yacc(Parser, debug=TRUE)
+})
