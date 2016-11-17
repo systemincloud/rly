ac3f30952c898151ff8ad88f94bc60d142bda221
mji <marek.jagielski@gmail.com>
2016-11-17 01:22:59 +0100

    prepare for CRAN

--- R/logger.R
+++ R/logger.R
@@ -1,9 +1,27 @@
+#' Print log message to file or console.
+#' 
 #' This object is a stand-in for a logging object created by the
-#' logging module.   RLY will use this by default to create things
-#' such as the parser.out file.  If a user wants more detailed
+#' logging module. RLY will use this by default to create things
+#' such as the parser.out file. If a user wants more detailed
 #' information, they can create their own logging object and pass
 #' it into RLY.
-
+#' 
+#' @usage
+#' debuglog <- RlyLogger$new()
+#' debuglog <- RlyLogger$new(dir=NA, name=NA)
+#' 
+#' @param dir  A Directory where the debug file will be written
+#' @param name The name of the log file
+#' 
+#' @docType class
+#' @importFrom R6 R6Class futile.logger
+#' @format A \code{\link{R6Class}} object
+#' 
+#' @export
+#' 
+#' @examples
+#' debuglog <- RlyLogger$new(".", "file.out")
+#' debuglog$info('This is info message')
 RlyLogger <- R6Class("RlyLogger",
     public = list(
         name = NA,
@@ -18,7 +36,22 @@ RlyLogger <- R6Class("RlyLogger",
     )
 )
 
-#' Null logger is used when no output is generated. Does nothing.
+#' Null logger is used when no output should be generated. 
+#'
+#' Does nothing.
+#' 
+#' @usage
+#' debuglog <- NullLogger$new()
+#' 
+#' @docType class
+#' @importFrom R6 R6Class
+#' @format A \code{\link{R6Class}} object
+#' 
+#' @export
+#' 
+#' @examples
+#' debuglog <- NullLogger$new()
+#' debuglog$info('This will not print')
 NullLogger <- R6Class("NullLogger",
     public = list(
         initialize = function(f) {
