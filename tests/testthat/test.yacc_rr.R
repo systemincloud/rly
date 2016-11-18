5964c64b9939668f7a0687657857f2630402ff0d
mji <marek.jagielski@gmail.com>
2016-11-18 22:57:16 +0100

    refactor loggers

--- tests/testthat/test.yacc_rr.R
+++ tests/testthat/test.yacc_rr.R
@@ -14,38 +14,39 @@ Parser <- R6Class("Parser",
                       c('right','UMINUS')),
     # dictionary of names
     names = new.env(hash=TRUE),
-    p_statement_assign = function(doc='statement : NAME EQUALS expression', t) {
-      names[t[1]] <- t[3]
+    p_statement_assign = function(doc='statement : NAME EQUALS expression', p) {
+      self$names[[as.character(p$get(2))]] <- p$get(4)
     },
-    p_statement_assign_2 = function(doc='statement : NAME EQUALS NUMBER', t) {
-      names[t[1]] = t[3]
+    p_statement_assign_2 = function(doc='statement : NAME EQUALS NUMBER', p) {
+      self$names[[as.character(p$get(2))]] <- p$get(4)
     },
-    p_statement_expr = function(doc='statement : expression', t) {
-      cat(t[1])
+    p_statement_expr = function(doc='statement : expression', p) {
+      cat(p$get(2))
+      cat('\n')
     },
     p_expression_binop = function(doc='expression : expression PLUS expression
-                                     | expression MINUS expression
-                                     | expression TIMES expression
-                                     | expression DIVIDE expression', t) {
-      if(t[2] == '+')      t[0] <- t[1] + t[3]
-      else if(t[2] == '-') t[0] <- t[1] - t[3]
-      else if(t[2] == '*') t[0] <- t[1] * t[3]
-      else if(t[2] == '/') t[0] <- t[1] / t[3]
+                                                  | expression MINUS expression
+                                                  | expression TIMES expression
+                                                  | expression DIVIDE expression', p) {
+      if(p$get(3) == 'PLUS') p$set(1, p$get(2) + p$get(4))
+      else if(p$get(3) == 'MINUS') p$set(1, p$get(2) - p$get(4))
+      else if(p$get(3) == 'TIMES') p$set(1, p$get(2) * p$get(4))
+      else if(p$get(3) == 'DIVIDE') p$set(1, p$get(2) / p$get(4))
     },
-    p_expression_uminus = function(doc='expression : MINUS expression %prec UMINUS', t) {
-      t[0] <- -t[2]
+    p_expression_uminus = function(doc='expression : MINUS expression %prec UMINUS', p) {
+      p$set(1, -p$get(3))
     },
-    p_expression_group = function(doc='expression : LPAREN expression RPAREN', t) {
-      t[0] <- t[2]
+    p_expression_group = function(doc='expression : LPAREN expression RPAREN', p) {
+      p$set(1, p$get(3))
     },
-    p_expression_number = function(doc='expression : NUMBER', t) {
-      t[0] <- t[1]
+    p_expression_number = function(doc='expression : NUMBER', p) {
+      p$set(1, p$get(2))
     },
-    p_expression_name = function(doc='expression : NAME', t) {
-      t[0] <- names[t[1]]
+    p_expression_name = function(doc='expression : NAME', p) {
+      p$set(1, self$names[[as.character(p$get(2))]])
     },
-    p_error = function(t) {
-      cat(sprintf("Syntax error at '%s'", t$value))
+    p_error = function(p) {
+      cat(sprintf("Syntax error at '%s'", p$value))
     }
   )
 )
@@ -53,7 +54,7 @@ Parser <- R6Class("Parser",
 test_that("reduce", {
   expect_output(rly::yacc(Parser, debug=TRUE), 
 "Generating LALR tables 
-WARN>  1 reduce/reduce conflict 
-WARN>  reduce/reduce conflict in state 21 resolved using rule (statement -> NAME EQUALS NUMBER) 
-WARN>  rejected rule (expression -> NUMBER) in state 21 ", fixed=TRUE)
+WARN .* 1 reduce/reduce conflict
+WARN .* reduce/reduce conflict in state 21 resolved using rule \\(statement -> NAME EQUALS NUMBER\\)
+WARN .* rejected rule \\(expression -> NUMBER\\) in state 21")
 })
