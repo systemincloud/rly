362cc2b2b62921f60bbd9dd93e76ab967bf44f7c
mji <marek.jagielski@gmail.com>
2016-12-18 13:46:12 +0100

    example : BASIC

--- examples/BASIC/basinterp.R
+++ examples/BASIC/basinterp.R
@@ -16,9 +16,10 @@ BasicInterpreter <- R6Class("BasicInterpreter",
     
     stat = NA,
     pc   = NA,
+    dc   = NA,
     
     data = NA,
-
+    
     # Initialize the interpreter. prog is a dictionary
     # containing (line,statement) mappings
     initialize = function(prog=NA) {
@@ -40,12 +41,54 @@ BasicInterpreter <- R6Class("BasicInterpreter",
     collect_data = function() {
       self$data <- list()
       for(lineno in self$stat) {
-        if(self$prog[[lineno]][1] == 'DATA') {
-          self$data <- self$data + self$prog[[lineo]][[2]]
+        if(self$prog[[as.character(lineno)]][1] == 'DATA') {
+          self$data <- self$data + self$prog[[as.character(lineo)]][[2]]
         }
       }
       self$dc <- 0
     },
+    # Check for end statements
+    check_end = function() {
+      has_end <- NULL
+      for(lineno in self$stat)
+        if(self$prog[[as.character(lineno)]][1] == 'END' && is.null(has_end))
+          has_end <- lineno
+          
+      if(is.null(has_end)) {
+        print("NO END INSTRUCTION")
+        self$error <- TRUE
+        return
+      }
+
+      if(has_end != lineno) {
+        print("END IS NOT LAST")
+        self$error <- TRUE
+      }
+    },
+    # Check loops
+    check_loops = function() {
+      for(pc in 1:length(self$stat)) {
+        lineno <- self$stat[[pc]]
+        if(self$prog[[as.character(lineno)]][1] == 'FOR') {
+          forinst <- self$prog[[as.character(lineno)]]
+          loopvar <- forinst[[2]]
+          broke <- FALSE
+          for(i in (pc + 1):length(self$stat))
+            if(self$prog[[as.character(self$stat[[i]])]][[1]] == 'NEXT') {
+              nextvar <- self$prog[[as.character(self$stat[[i]])]][[2]]
+              if(nextvar != loopvar) next
+              self$loopend[[as.character(pc)]] <- i
+              broke <- TRUE
+              break
+            }
+          
+          if(!broke) {
+            print(sprintf("FOR WITHOUT NEXT AT LINE %s", self$stat[[as.character(pc)]]))
+            self$error <- TRUE
+          }
+        }
+      }
+    }, 
     # Run it
     run = function() {
       self$vars    <- new.env(hash=TRUE) # All variables
@@ -56,15 +99,15 @@ BasicInterpreter <- R6Class("BasicInterpreter",
       self$gosub   <- NA                 # Gosub return point (if any)
       self$error   <- FALSE              # Indicates program error
       
-      self$stat <- list(self$prog) # Ordered list of all line numbers
-#      self$stat.sort()
+      self$stat <- names(self$prog) # Ordered list of all line numbers
+      self$stat <- sort(sapply(self$stat, strtoi))
       self$pc <- 0                 # Current program counter
-      
+
       # Processing prior to running
       
-#      self$collect_data()          # Collect all of the data statements
-#      self$check_end()
-#      self$check_loops()
+      self$collect_data()          # Collect all of the data statements
+      self$check_end()
+      self$check_loops()
       
     }
   )
